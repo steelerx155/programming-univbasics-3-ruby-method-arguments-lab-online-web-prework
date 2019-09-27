8bc2678d60cf22cc4ac0eaae3f91b0dc9f063832
Randy Steele <steelerx155@gmail.com>
2019-09-27 02:26:51 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -1,4 +1,4 @@
-#def introduction(name)
+# def introduction(name)
 #  puts "Hi, my name is #{name}."
 #end
 
@@ -6,9 +6,9 @@
 #  puts "Hi, my name is #{name} and I am learning to program in #{language}."
 #end
 
-#def introduction_with_language_optional(name, language="Ruby")
-#  puts "Hi, my name is #{name} and I am learning to program in #{language}."
-#end
+def introduction_with_language_optional(name, language="Ruby")
+  puts "Hi, my name is #{name} and I am learning to program in #{language}."
+end
 
 
 
